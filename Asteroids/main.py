4b98a99a14e4e205553774b0f9a28ed546247a7a
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-03 17:54:09 -0400

    set fps to 60

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -9,12 +9,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    fpsclock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): # allows to quit the game
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0)) # fill screen black
         pygame.display.flip()
+        fpsclock.tick(60)
+        dt = fpsclock.get_time() / 1000
 
 if __name__ == "__main__":
     main()
