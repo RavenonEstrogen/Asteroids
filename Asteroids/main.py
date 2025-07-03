0076447b7d861b23c11570b07352b4ca7668bb3d
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-02 21:07:35 -0400

    initialize pygame

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,8 +1,15 @@
 import pygame
 # allows to use code from open-source pygame library
+from constants import *
+# allows to use constants from the file
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
 
 if __name__ == "__main__":
