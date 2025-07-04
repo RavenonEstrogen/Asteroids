55d07054f84438d055528bc01442d6a69f5f7f73
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-04 15:17:18 -0400

    creates groups instead of using objects

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -7,11 +7,11 @@ from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     fpsclock = pygame.time.Clock()
@@ -23,8 +23,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0)) # fill screen black
 
-        player.draw(screen) # draw player
-        player.update(dt)
+        for draws in drawable:
+            draws.draw(screen) # draw player
+        updatable.update(dt)
 
         pygame.display.flip()
         fpsclock.tick(60)
