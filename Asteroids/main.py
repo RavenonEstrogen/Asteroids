302351dace7ce69bb68708422ee7e75f78859537
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-04 20:30:00 -0400

    player can now shoot

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,5 +1,6 @@
 import pygame
 # allows to use code from open-source pygame library
+import sys
 from constants import *
 # allows to use constants from the file
 from player import Player
@@ -8,6 +9,7 @@ from asteroid import Asteroid
 # allows to use Asteroids class
 from asteroidfield import AsteroidField
 #
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,9 +18,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     asteroidfield = AsteroidField()
 
@@ -34,6 +38,11 @@ def main():
         for draws in drawable:
             draws.draw(screen) # draw player and asteroids
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
         fpsclock.tick(60)
--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,8 +1,9 @@
 from circleshape import *
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
 
 class Player(CircleShape):
-    def __init__(self, x, y, PLAYER_RADIUS):
+    def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
@@ -32,7 +33,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
