51640f1c1ad8c5f915af406150e319088d6cf6e5
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-04 23:06:58 -0400

    adds timer between shots and destroys asteroids

--- Asteroids/asteroid.py
+++ Asteroids/asteroid.py
@@ -1,4 +1,7 @@
+import pygame
+import random
 from circleshape import *
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +12,11 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        first_split = pygame.vetcor2.rotate(self.velocity, random_angle)
+        second_split = pygame.vector2.rotate(self.velocity, -random_angle)
--- Asteroids/main.py
+++ Asteroids/main.py
@@ -43,6 +43,12 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         pygame.display.flip()
         fpsclock.tick(60)
